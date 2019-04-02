0d41827073a557cab5ae6e09692fef05b366943e
Huffmajo <huffmajo@oregonstate.edu>
2019-04-01 21:00:30 -0700

    Added bash script to run benchmarks with different thread counts. Added print outs of benchmark data for work done to project0.c.

--- project0.c
+++ project0.c
@@ -1,10 +1,17 @@
+/***********************************************************
+ * Program: project0.c
+ * Author: Joel Huffman
+ * Last updated: 4/1/2019
+ * Sources: https://oregonstate.instructure.com/courses/1716463/pages/week-01-listen-to-this?module_item_id=18618494
+ * https://www.gamedev.net/forums/topic/392211-max-value-for-double-/
+ ***********************************************************/
 #include <omp.h>
 #include <stdio.h>
 #include <math.h>
+#include <float.h>
 
-#define NUMT	         4
-#define ARRAYSIZE       200
-#define NUMTRIES        200
+#define NUMTRIES 200
+#define ARRAYSIZE 10000000
 
 float A[ARRAYSIZE];
 float B[ARRAYSIZE];
@@ -19,12 +26,15 @@ main()
 #endif
 
 	// find out how many cores are on system
-	printf("Number of system cores: %d\n", omp_get_num_procs());
+	printf("\nNumber of system cores: %d\n", omp_get_num_procs());
 
 	omp_set_num_threads(NUMT);
-	fprintf(stderr, "Using %d threads\n", NUMT);
+	printf("Using %d threads with sample size of %d\n", NUMT, NUMTRIES);
 
 	double maxMegaMults = 0.;
+	double sumMegaMults = 0.;
+	double fastestTime = DBL_MAX;
+	double sumFastestTime = 0.;
 
 	for (int t = 0; t < NUMTRIES; t++)
 	{
@@ -38,14 +48,26 @@ main()
 
 		double time1 = omp_get_wtime();
 		double megaMults = (double)ARRAYSIZE / (time1 - time0) / 1000000.;
+		double elapsedTime = (time1 - time0) * 1000000;
+		
 		if (megaMults > maxMegaMults)
+		{
 			maxMegaMults = megaMults;
+		}
+
+		if (elapsedTime < fastestTime)
+		{
+			fastestTime = elapsedTime;
+		}
+
+		sumMegaMults += megaMults;
+		sumFastestTime += fastestTime;
+
 	}
 
 	printf("Peak Performance = %8.2lf MegaMults/Sec\n", maxMegaMults);
-
-	// note: %lf stands for "long float", which is how printf prints a "double"
-	//        %d stands for "decimal integer", not "double"
-
+	printf("Average Performance = %8.2lf MegaMults/Sec\n", (sumMegaMults / (double) NUMTRIES));
+	printf("Fastest Time = %8.2lf μsec\n", fastestTime);
+	printf("Average Time = %8.2lf μsec\n", (sumFastestTime / (double) NUMTRIES));
 	return 0;
 }
