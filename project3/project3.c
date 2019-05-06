ab654034307bf731ff052b9dcb32c1a81ff4afa9
Huffmajo <huffmajo@oregonstate.edu>
2019-05-05 21:31:07 -0700

    Added custom sasquatch agent. Everything working as intended.

--- project3/project3.c
+++ project3/project3.c
@@ -18,6 +18,7 @@ float	NowPrecip;		// inches of rain per month
 float	NowTemp;		// temperature this month
 float	NowHeight;		// grain height in inches
 int	NowNumDeer;		// number of deer in the current population
+int	NowNumSasquatch;	// number of sasquatch in the current population
 
 const float GRAIN_GROWS_PER_MONTH =		8.0;
 const float ONE_DEER_EATS_PER_MONTH =		0.5;
@@ -33,6 +34,8 @@ const float RANDOM_TEMP =			10.0;	// plus or minus noise
 const float MIDTEMP =				40.0;
 const float MIDPRECIP =				10.0;
 
+const float ONE_SASQUATCH_EATS_PER_MONTH =	1.5;
+
 // returns squared result of provided float
 float SQR( float x )
 {
@@ -44,17 +47,19 @@ float RandFloat( unsigned int *seedp,  float low, float high )
 {
         float r = (float) rand_r( seedp );              // 0 - RAND_MAX
 
-        return(   low  +  r * ( high - low ) / (float)RAND_MAX   );
+        return(low + r * (high - low) / (float)RAND_MAX);
 }
 
 // returns random integer within low to high range
+/*
 int RandInt( unsigned int *seedp, int ilow, int ihigh )
 {
         float low = (float)ilow;
         float high = (float)ihigh + 0.9999f;
 
-        return (int)(  RandFloat(seedp, low,high) );
+        return (int)(RandFloat(seedp, low,high) );
 }
+*/
 
 // manages deer thread
 void GrainDeer()
@@ -118,6 +123,7 @@ void Grain()
 		// compute next grain height
 		nextHeight = NowHeight + (tempFactor * precipFactor * GRAIN_GROWS_PER_MONTH);
 		nextHeight -= (float)NowNumDeer * ONE_DEER_EATS_PER_MONTH;
+		nextHeight -= (float)NowNumSasquatch * ONE_SASQUATCH_EATS_PER_MONTH;
 
 		// can't have negative height
 		if (nextHeight < 0.)
@@ -146,6 +152,7 @@ void Grain()
 // updates timing and prints results of other threads
 void Watcher()
 {
+	unsigned int seed = 0;
 	while( NowYear < 2025 )
 	{
 		// compute a temporary next-value for this quantity
@@ -161,13 +168,29 @@ void Watcher()
 		// DoneAssigning barrier:
 		#pragma omp barrier
 
+		// calculate and update weather
+		float ang = (  30.*(float)NowMonth + 15.  ) * ( M_PI / 180. );
+
+		float temp = AVG_TEMP - AMP_TEMP * cos( ang );
+		NowTemp = temp + RandFloat( &seed, -RANDOM_TEMP, RANDOM_TEMP );
+
+		float precip = AVG_PRECIP_PER_MONTH + AMP_PRECIP_PER_MONTH * sin( ang );
+		NowPrecip = precip + RandFloat( &seed,  -RANDOM_PRECIP, RANDOM_PRECIP );
+		if( NowPrecip < 0. )
+			NowPrecip = 0.;	
+
+		//convert to metric for printing
+		float metricHeight = NowHeight * 2.54;
+		float metricPrecip = NowPrecip * 2.54;
+		float metricTemp = (NowTemp - 32) * (5. / 9.);
+
 		// print results for this month
-		printf("%d/%d\t%d\t%lf\t%lf\t%lf\n", NowMonth + 1, NowYear, NowNumDeer, NowHeight, NowPrecip, NowTemp);
+		printf("%d/%d\t%d\t%d\t%lf\t%lf\t%lf\n", NowMonth + 1, NowYear, NowNumDeer, NowNumSasquatch, metricHeight, metricPrecip, metricTemp);
 
 		// write results to .txt file as well
 		FILE *fp;
 		fp = fopen("results.txt", "a");
-		fprintf(fp, "%d/%d\t%d\t%lf\t%lf\t%lf\n", NowMonth + 1, NowYear, NowNumDeer, NowHeight, NowPrecip, NowTemp);
+		fprintf(fp, "%d/%d\t%d\t%d\t%lf\t%lf\t%lf\n", NowMonth + 1, NowYear, NowNumDeer, NowNumSasquatch, metricHeight, metricPrecip, metricTemp);
 		fclose(fp);
 
 		// increment month
@@ -180,18 +203,6 @@ void Watcher()
 			NowMonth = 0;
 		}
 
-		// calculate and update weather
-		float ang = (  30.*(float)NowMonth + 15.  ) * ( M_PI / 180. );
-
-		float temp = AVG_TEMP - AMP_TEMP * cos( ang );
-		unsigned int seed = 0;
-		NowTemp = temp + RandFloat( &seed, -RANDOM_TEMP, RANDOM_TEMP );
-
-		float precip = AVG_PRECIP_PER_MONTH + AMP_PRECIP_PER_MONTH * sin( ang );
-		NowPrecip = precip + RandFloat( &seed,  -RANDOM_PRECIP, RANDOM_PRECIP );
-		if( NowPrecip < 0. )
-			NowPrecip = 0.;	
-
 		// DonePrinting barrier:
 		#pragma omp barrier
 
@@ -199,21 +210,38 @@ void Watcher()
 	}
 }
 
-// manages blood moon (custom agent) thread
-void BloodMoon()
+// manages sasquatch (custom agent) thread
+void Sasquatch()
 {
 	while( NowYear < 2025 )
 	{
 		// compute a temporary next-value for this quantity
 		// based on the current state of the simulation:
 
-		//***INSERT COMPUTING HERE
+		int nextNumSasquatch;
+		
+		// if the deer-to-sasquatch ratio is over 2:1, increment sasquatch
+		if (NowNumDeer > NowNumSasquatch * 2)
+		{
+			nextNumSasquatch = NowNumSasquatch + 1;
+		}
+
+		// if there are fewer than 2:1 deer-to-sasquatch, remove all sasquatch
+		else if (NowNumDeer < NowNumSasquatch * 2)
+		{
+			nextNumSasquatch = 0;
+
+			if (nextNumSasquatch < 0)
+			{
+				nextNumSasquatch = 0;
+			}
+		}
 
 		// DoneComputing barrier:
 		#pragma omp barrier
 
 		// copy next state into now state
-		//***INSERT UPDATE TO CURRENT STATE HERE
+		NowNumSasquatch = nextNumSasquatch;
 
 		// DoneAssigning barrier:
 		#pragma omp barrier
@@ -242,8 +270,9 @@ int main( int argc, char *argv[ ] )
 	NowYear  = 2019;
 
 	// starting state (feel free to change this if you want):
-	NowNumDeer = 5;
-	NowHeight =  2.5;
+	NowNumDeer = 2;
+	NowHeight =  1.5;
+
 
 	#pragma omp parallel sections
 	{
@@ -264,7 +293,7 @@ int main( int argc, char *argv[ ] )
 
 		#pragma omp section
 		{
-			BloodMoon( );	
+			Sasquatch( );	
 		}
 
 	}       // implied barrier -- all functions must return in order
