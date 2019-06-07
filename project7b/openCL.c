91793a71717a8aea1967e8636f56dbf15c4b6e29
Huffmajo <huffmajo@oregonstate.edu>
2019-06-06 21:44:48 -0700

    Fixed minor openCL error. Added results

--- project7b/openCL.c
+++ project7b/openCL.c
@@ -196,7 +196,7 @@ main( int argc, char *argv[ ] )
 
 	// 12. read the results buffer back from the device to the host:
 
-	status = clEnqueueReadBuffer( cmdQueue, dSums, CL_TRUE, 0, 2*Size*sizeof(cl_float), hSums, 0, NULL, NULL );
+	status = clEnqueueReadBuffer( cmdQueue, dSums, CL_TRUE, 0, Size*sizeof(cl_float), hSums, 0, NULL, NULL );
 	if( status != CL_SUCCESS )
 		fprintf( stderr, "clEnqueueReadBuffer failed\n" );
 
